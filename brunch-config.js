ef1ea6021b8d68eb0a9ee97b60c470d2fe44de9a
Jason Stiebs <jason@rokkincat.com>
2016-04-29 09:55:37 -0500

    Have elm working as embedded but I need to also get elm-brunch working somehow

--- brunch-config.js
+++ brunch-config.js
@@ -51,8 +51,17 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
-    }
+      ignore: [/web\/static\/vendor/, /web\/static\/elm/]
+    },
+//    elmBrunch: {
+//      // Set to path where elm-package.json is located, defaults to project root (optional)
+//      // if your elm files are not in /app then make sure to configure paths.watched in main brunch config
+//      elmFolder: 'web/static/elm',
+//      // Set to the elm file(s) containing your "main" function
+//      // `elm make` handles all elm dependencies (required)
+//      // relative to `elmFolder`
+//      mainModules: ['Main.elm']
+//    }
   },
 
   modules: {
